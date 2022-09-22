1169505e4d182d6903500d7e9ba07baab46b6226
Jatin-CI0002 <jatin@caizin.com>
2022-09-22 16:36:10 +0530

    updated approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chat-app';
+  user : string = '';
+  userName:string = '';
+  Users:Array<string> = [];
+
+  onKeydown(event: { key: string; }) {
+    if (event.key === "Enter") {
+      this.createUser();
+    }
+  }
+  createUser(){
+  if(this.Users.includes(this.user))
+  {
+    alert("User is already in Chat");
+    return;
+  }
+  else if(!this.user){
+    alert("Please Enter UserName");
+    return;
+  }
+  else{
+    this.userName = this.user;
+    this.Users.push(this.userName);
+    this.user = '';
+  }
+
+}
 }
